0d6e27f09db6a5b70f8f28ffc2992365cf227615
Pablo X <jprodsanz@gmail.com>
2023-02-06 20:17:20 -0600

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (   String[] args ){
+        System.out.println("My name is... Chicka, chicka, chicka, Slim Shady");
+        System.out.println("I am 50 years old");
+        System.out.println("From Detroit Michigan");
+    }
+}
